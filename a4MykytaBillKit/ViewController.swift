ee8448a8b1840d47d5ffd3b5906d9ba31f859611
Mykyta Varnikov <varnikov@sheridancollege.ca>
2025-04-12 19:09:55 -0400

    Initial Commit

---
+++ a4MykytaBillKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  a4MykytaBillKit
+//
+//  Created by Mykyta Kaisenberg on 2025-04-12.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
